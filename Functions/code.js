fe3a9ed2f2f516c164cbe6e7fe6e578ec1d33dbb
unknown <muhammadrohan461@gmail.com>
2022-07-31 22:44:10 +0500

    new files added

---
+++ Functions/code.js
@@ -0,0 +1,15 @@
+import {compose , pipe} from 'lodash'
+
+console.log("Hello World!");
+function sayHello()
+{
+    return "hello world"
+}
+let fn = sayHello; 
+ fn()
+
+ function greet()
+ {
+    console.log()
+ }
+  
